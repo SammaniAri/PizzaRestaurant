6d0be47331a98c734f8f5bd973e56595a74b2ce5
Sammani Ariyarathne <sammani.ari@gmail.com>
2024-01-24 09:36:39 +0100

    UI of the Navbar and pages

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from "react-router-dom";
 
 function App() {
-  return (
+	return (
 		<div className="App">
-			<header className="App-header">
-				<img
-					src={logo}
-					className="App-logo"
-					alt="logo"
-				/>
-				<p>
-					Edit <code>src/App.js</code>{" "}
-					and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
-				<h1 className="text-3xl font-bold underline">
-					Hello world!
-				</h1>
-			</header>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route
+						path="/"
+						element={<Home />}
+					/>
+				</Routes>
+				<Footer />
+			</Router>
 		</div>
 	);
 }
