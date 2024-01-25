d42b1b0ae8b5f560f32639aa6cf4a8570849048f
Sammani Ariyarathne <sammani.ari@gmail.com>
2024-01-25 16:50:22 +0100

    rough design of home page and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
 /** @format */
 
 import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div class="h-32 pt-8">
+			<div className="socialMedia">
+				<InstagramIcon fontSize="large" />
+				<LinkedInIcon fontSize="large" />
+				<GitHubIcon fontSize="large" />
+			</div>
+			<p> &copy; 2024 SamsPizza.com</p>
+		</div>
+	);
 };
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,35 @@
 /** @format */
 
 import React from "react";
+import { Link } from "react-router-dom";
+import headerImage from "../assets/Steaming-slice.svg";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div>
+			<div
+				class="bg-no-repeat bg-center bg-cover h-screen"
+				style={{
+					backgroundImage: `url(${headerImage})`,
+				}}>
+				<div class="pt-16">
+					<h1 class=" pt-48 pb-2 font-semibold leading-none tracking-tight text-gray-900 text-3xl md:text-5xl lg:text-6xl dark:text-white">
+						Sam's Pizza
+					</h1>
+					<h2 class="pb-16 text-2xl md:text-4xl lg:text-5xl font-normal dark:text-white">
+						Pizza to fit any taste
+					</h2>
+					<Link>
+						<button
+							type="button"
+							class="text-white bg-[#050708] hover:bg-[#BA4918]/80 focus:ring-2 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#050708]/40 dark:focus:ring-gray-600 me-2 mb-2 p-2">
+							Order Now
+						</button>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
