60e99cd107a117599c4ea8bd63460f0254d8bf79
Sammani Ariyarathne <sammani.ari@gmail.com>
2024-02-06 23:50:17 +0100

    close hamburger menu when a page is clicked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,11 @@ const Header = () => {
 		setHamburger(!hamburger);
 	};
 
+	const handleCloseMenu = () => {
+		setHamburger(false);
+	};
+
+
 	return (
 		<div className="bg-[#050708] sticky top-0 z-[20] mx-auto flex w-full items-center justify-between">
 			<div className="hamburger md:hidden fixed top-4 left-4 z-50">
@@ -31,27 +36,32 @@ const Header = () => {
 				}`}>
 				<Link
 					to="/"
-					className="hover:bg-[#A20000] font-bold">
+					className="hover:bg-[#A20000] font-bold"
+					onClick={handleCloseMenu}>
 					Home
 				</Link>
 				<Link
 					to="/menu"
-					className="hover:bg-[#A20000]">
+					className="hover:bg-[#A20000]"
+					onClick={handleCloseMenu}>
 					Menu
 				</Link>
 				<Link
 					to="/about"
-					className="hover:bg-[#A20000]">
+					className="hover:bg-[#A20000]"
+					onClick={handleCloseMenu}>
 					About
 				</Link>
 				<Link
 					to="/contact"
-					className="hover:bg-[#A20000]">
+					className="hover:bg-[#A20000]"
+					onClick={handleCloseMenu}>
 					Contact
 				</Link>
 				<Link
 					to="/authpage"
-					className="hover:text-[#A20000]">
+					className="hover:text-[#A20000]"
+					onClick={handleCloseMenu}>
 					Log in
 				</Link>
 				<Logout></Logout>
