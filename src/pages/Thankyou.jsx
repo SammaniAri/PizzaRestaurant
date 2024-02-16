f668d7f9d80465b4935ab3c4a656d3af6f728e15
Sammani Ariyarathne <sammani.ari@gmail.com>
2024-02-16 17:03:33 +0100

    Thank you page

--- src/pages/Thankyou.jsx
+++ src/pages/Thankyou.jsx
@@ -1,9 +1,26 @@
 /** @format */
 
 import React from "react";
+import thankyouImage from "../assets/steaming.svg";
 
 const Thankyou = () => {
-	return <div>Thankyou</div>;
+	return (
+		<div
+			className="bg-no-repeat bg-center bg-cover h-screen text-black flex flex-col justify-center items-center "
+			style={{
+				backgroundImage: `url(${thankyouImage})`,
+			}}>
+			<div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 bg-[#E1DAC7]">
+				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+					Thank you for ordering our
+					pizzas!
+				</h5>
+				<p class="font-normal text-gray-700 dark:text-gray-400">
+					We hope you enjoy them!
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Thankyou;
