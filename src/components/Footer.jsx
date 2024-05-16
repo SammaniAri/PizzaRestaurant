a1aa36a311136f1a72ddf8371908063a00f11b8c
Sammani Ariyarathne <sammani.ari@gmail.com>
2024-05-17 00:59:03 +0200

    memorizing header & footer for the purpose of optimizing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
-const Footer = () => {
+const Footer = React.memo(() => {
 	return (
 		<div className="h-32 pt-8 bg-black">
 			<div className="socialMedia flex justify-center">
@@ -37,6 +37,6 @@ const Footer = () => {
 			<p> &copy; 2024 SamsPizza.com</p>
 		</div>
 	);
-};
+});
 
 export default Footer;
